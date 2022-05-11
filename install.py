4419b0264d50fb9648bb00518d36c43d9abd560c
MPotono@gmail.com <MPotono@gmail.com>
2022-05-11 10:17:00 +0200

    version: 1.1.0

--- install.py
+++ install.py
@@ -2,12 +2,6 @@ import os
 import platform
 
 if __name__ == '__main__':
-    def clear_console():
-        if platform.system() == 'Linux':
-            os.system('clear')
-        elif platform.system() == 'Windows':
-            os.system('cls')
-
     if platform.system() == 'Linux':
         os.system('mkdir Downloads')
         os.system('mkdir Statistics')
